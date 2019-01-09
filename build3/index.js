43556fc134df8ac31d09d946f3889bdaf94a3466
VadimRydenko <rudenkovvv@ukr.net>
2019-01-09 19:17:46 +0200

    build3

--- build3/index.js
+++ build3/index.js
@@ -9050,6 +9050,98 @@ module.exports = function (regExp, replace) {
 
 __webpack_require__(329);
 
+function aсcordion() {
+    var ACTIVE_CLASSNAME = 'leftblock-menu__container_open';
+    var container = document.querySelectorAll('.leftblock-menu__container');
+
+    for (var i = 0; i < container.length; i++) {
+        container[i].onclick = toggle;
+    }
+    function toggle() {
+        switchOff();
+        // if (this.container.classList.contain(ACTIVE_CLASSNAME)) {
+        //     close ();
+        // }else {
+        //     open ();
+        // }
+        // if (this.classList.contains(ACTIVE_CLASSNAME)) {
+        //     close ();
+        // }else {
+        //     open ();
+        // }
+        this.classList.add(ACTIVE_CLASSNAME);
+    }
+    function switchOff() {
+        for (var _i = 0; _i < container.length; _i++) {
+            container[_i].classList.remove(ACTIVE_CLASSNAME);
+        }
+    }
+
+    // function close () {
+    //     this.classList.remove(ACTIVE_CLASSNAME);
+    // }
+    // function open () {
+    //     this.classList.add(ACTIVE_CLASSNAME);
+    // }
+}
+
+aсcordion();
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+
+
+//     if (n > slides.length) {
+//     slideIndex = 1
+//     }
+//     if (n <  1) {
+//     slideIndex=slides.lenght
+//     }
+//     for (i=0; i <slides.lenght ; i++){
+//     slides[i].style.display= "none";
+//     }
+//     slides[slideIndex-1].style.display="block";
+// }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
 /***/ }),
 /* 329 */
 /***/ (function(module, exports) {
